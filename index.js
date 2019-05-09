212110961aeac0efe3c2d05b2df9261215a9fb06
Florian Märkl <info@florianmaerkl.de>
2019-05-09 15:18:00 +0200

    Add std::deque

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
 import StdVector from "./lib/std_vector";
 import StdString from "./lib/std_string";
-export { StdVector, StdString };
+import StdDeque from "./lib/std_deque";
+export { StdVector, StdString, StdDeque };
